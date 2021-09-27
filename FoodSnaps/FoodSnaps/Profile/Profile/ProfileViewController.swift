d9a2ab99b82c48e3d16390d7f2f78ad851ecb0f0
Malsha Hansini <malsha.hansini94@gmail.com>
2021-09-27 10:40:43 +0530

    add avatar

--- FoodSnaps/FoodSnaps/Profile/Profile/ProfileViewController.swift
+++ FoodSnaps/FoodSnaps/Profile/Profile/ProfileViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {    
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var imgProfilePic: UIImageView!
+    
     override func viewWillAppear(_ animated: Bool) {
         configUI()
     }
     
     private func configUI() {
+        configProfilePic()
+    }
+    
+    private func configProfilePic() {
+        imgProfilePic.layer.cornerRadius = 40
+        imgProfilePic.layer.borderWidth = 2
+        imgProfilePic.layer.borderColor = UIColor.init(named: "avatarBorder")?.cgColor
+        imgProfilePic.layer.masksToBounds = true
     }
 }
